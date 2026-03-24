5e2445dbc6d5859f505a362894b326035df31e6a
smithjoshg-design <smithjoshg@gmail.com>
2026-03-23 21:20:10 -0400

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Analytics } from '@vercel/analytics/react'
 import LeadflowMarketingSiteV2 from './LeadflowMarketingSiteV2'
 
 export default function App() {
-  return <LeadflowMarketingSiteV2 />
+  return (
+    <>
+      <LeadflowMarketingSiteV2 />
+      <Analytics />
+    </>
+  )
 }
